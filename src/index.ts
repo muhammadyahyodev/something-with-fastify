e44f325fd756026786e6ebdb38cef9ba40e26011
yakhyo <muhammadyahyodev@gmail.com>
2024-01-10 22:03:11 +0500

    connect to db & API

---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import { fastify } from 'fastify';
+import pino from 'pino';
+import dotenv from 'dotenv';
+import blogRoutes from './routes/blog-routes';
+import db from './config/index';
+
+dotenv.config();
+
+const port = process.env.PORT || 7000;
+const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/fastify';
+
+const server = fastify({
+    logger: pino({ level: 'info' })
+});
+
+// Activate plugins below:
+server.register(db, { uri });
+server.register(blogRoutes);
+
+const start = async () => {
+  try {
+
+
+      await server.listen(port, () => {
+        console.log(`\nServer has been started on ${port} port...`)
+      });
+  } catch (err) {
+      server.log.error(err);
+      process.exit(1);
+  }
+};
+
+start();
---
+++ src/routes/blog-routes.ts
@@ -0,0 +1,58 @@
+import { FastifyInstance, FastifyPluginOptions, FastifyPluginAsync } from 'fastify';
+import fp from 'fastify-plugin';
+import { Db } from '../config/index';
+import { BlogAttrs } from '../config/models/blog-model';
+  
+// Declaration merging
+declare module 'fastify' {
+    export interface FastifyInstance {
+        db: Db;
+    }
+}
+
+interface blogParams {
+    id: string;
+}
+
+const BlogRoute: FastifyPluginAsync = async (server: FastifyInstance, options: FastifyPluginOptions) => {
+
+    server.get('/blogs', {}, async (request, reply) => {
+        try {
+            const { Blog } = server.db.models;
+            const blogs = await Blog.find({});
+            return reply.code(200).send(blogs);
+        } catch (error) {
+            request.log.error(error);
+            return reply.send(500);
+        }
+    });
+
+    server.post<{ Body: BlogAttrs }>('/blogs', {}, async (request, reply) => {
+        try {
+            const { Blog } = server.db.models;
+            const blog = await Blog.addOne(request.body);
+            await blog.save();
+            return reply.code(201).send(blog);
+        } catch (error) {
+            request.log.error(error);
+            return reply.send(500);
+        }
+    });
+
+    server.get<{ Params: blogParams }>('/blogs/:id', {}, async (request, reply) => {
+        try {
+            const ID = request.params.id;
+            const { Blog } = server.db.models;
+            const blog = await Blog.findById(ID);
+            if (!blog) {
+                return reply.send(404);
+            }
+            return reply.code(200).send(blog);
+        } catch (error) {
+            request.log.error(error);
+            return reply.send(400);
+        }
+    });
+};
+
+export default fp(BlogRoute);
